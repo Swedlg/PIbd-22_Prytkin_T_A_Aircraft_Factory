e7ad9edf8ea76855639fc6064026c641154cc5d7
Swedlg <swedlg@yandex.ru>
2021-05-09 11:40:11 +0400

    fixrenamingprojects

--- AbstractJewelryShopView/Properties/AssemblyInfo.cs
+++ AbstractJewelryShopView/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
-[assembly: AssemblyTitle("AbstractAircraftFactoryView")]
+[assembly: AssemblyTitle("AbstractJewelryShopView")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AbstractAircraftFactoryView")]
+[assembly: AssemblyProduct("AbstractJewelryShopView")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
